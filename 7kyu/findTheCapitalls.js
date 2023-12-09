1db229a39026a4c99ece12d6a187e1493f9d59c4
Hailo7ts <101282320+Hailo7ts@users.noreply.github.com>
2023-12-09 12:33:12 -0700

    Create findTheCapitalls.js

---
+++ 7kyu/findTheCapitalls.js
@@ -0,0 +1,18 @@
+/**
+ * Find the capitals
+ * Write a function that takes a single string (word) as argument. The 
+ * function must return an ordered list containing the indexes of all 
+ * capital letters in the string.
+ */
+
+var capitals = function (word) {
+	let arr = []
+  
+  for(let i=0; i<word.length; i++){
+    if(word.charAt(i) === word.charAt(i).toUpperCase()){
+      arr.push(i)
+    }
+  }
+  
+  return arr
+};

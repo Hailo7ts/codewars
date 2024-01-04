bf5cabe0db2fa2e114daa74ef7200221f5a688ea
Hailo7ts <101282320+Hailo7ts@users.noreply.github.com>
2024-01-04 13:28:53 -0700

    Create roundUpToTheNextMultipleOf5.js

---
+++ 7kyu/roundUpToTheNextMultipleOf5.js
@@ -0,0 +1,6 @@
+/**
+ * Round up to the next multiple of 5
+ * Given an integer as input, can you round it to the next (meaning, 
+ * "greater than or equal") multiple of 5?
+ */
+const roundToNext5 = (n)=> Math.ceil(n/5) *5

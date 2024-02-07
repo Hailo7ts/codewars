523c72c830f21805c63a1783eba7f557eab3110e
Hailo7ts <101282320+Hailo7ts@users.noreply.github.com>
2024-02-07 12:18:03 -0700

    Create duplicateEncoder.js

---
+++ 6kyu/duplicateEncoder.js
@@ -0,0 +1,23 @@
+/**
+ * Duplicate Encoder
+ * The goal of this exercise is to convert a string to a new string where 
+ * each character in the new string is "(" if that character appears only 
+ * once in the original string, or ")" if that character appears more than
+ * once in the original string. Ignore capitalization when determining if 
+ * a character is a duplicate.
+ */
+
+function duplicateEncode(word){
+    let str = ''
+    word = word.toLowerCase()
+    
+    for(let i=0; i<word.length; i++){
+      if(word.indexOf(word[i]) === word.lastIndexOf(word[i])){
+        str += '('
+      }else{
+        str += ')'
+      }
+    }
+    
+    return str
+  }
